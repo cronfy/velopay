4a635b88134777f2734f589db88274d444147e23
cronfy <cronfy@gmail.com>
2018-01-06 17:37:23 +0300

    Яндекс.Касса: Добавлен вариант оплаты через Сбербанк.

---
+++ src/gateways/YandexKassaSberbankApi/Gateway.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cronfy
+ * Date: 01.11.17
+ * Time: 13:36
+ */
+
+namespace cronfy\velopay\gateways\YandexKassaSberbankApi;
+
+use cronfy\velopay\gateways\YandexKassaApi\BaseGateway;
+
+class Gateway extends BaseGateway {
+    public $payment_method = 'sberbank';
+}
