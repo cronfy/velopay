876eb9c36a58e5664637b3efb6ee16b742dc2ec0
cronfy <cronfy@gmail.com>
2022-05-24 22:20:58 +0400

    yandex_money -> yoomoney fix

--- src/gateways/YandexKassaApi/BaseGateway.php
+++ src/gateways/YandexKassaApi/BaseGateway.php
@@ -241,7 +241,7 @@ abstract class BaseGateway extends AbstractGateway
         if ($this->testMode) {
             throw new Exception("Not implemented");
         }
-        return 'https://api.yookassa.ru/api/v3/';
+        return 'https://api.yookassa.ru/v3/';
     }
 
     /**
